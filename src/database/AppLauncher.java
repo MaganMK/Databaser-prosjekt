302af3604eeaea341ebf47c7864c5f577ad21fb2
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2018-03-07 14:01:59 +0100

    copypaste

--- src/database/AppLauncher.java
+++ src/database/AppLauncher.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-// Klasse for a kjore manager.fxml
 public class AppLauncher extends Application {
 	
 	@Override
