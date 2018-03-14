40d5cd31646d0f900078f6b58e627b1d9d1e6cfc
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2018-03-14 12:36:44 +0100

    fxml til opg 1 + controllerting

--- src/controllers/RegistrereOvelseController.java
+++ src/controllers/RegistrereOvelseController.java
@@ -1,5 +1,48 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class RegistrereOvelseController {
+	
+	//Legg til nytt apparat
+	@FXML TextField newApparat;
+	
+	@FXML public void addApparat() {
+		
+	}
+	
+	
+	//Legg til ny øvelse
+	@FXML TextField newOvelse;
+	
+	@FXML public void addOvelse() {
+		
+	}
+	
+	
+	//Lag ny treningsøkt
+	@FXML TextField newSession;
+	
+	@FXML public void addSession() {
+		
+	}
+	
+	
+	//Legg øvelse i treningsøkt
+	@FXML TextField sessionField,
+					exerciseField,
+					setField,
+					weightField;
+	
+	@FXML public void addExerciseToSession() {
+		
+	}
+	
+	
+	//Legge øvelse til apparat
+	@FXML public void addExerciseToApparat() {
+		
+	}
 
 }
