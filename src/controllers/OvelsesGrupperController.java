5d3f5b74a0df2686379c47e57fcd07bc52d8bc6f
MaganMK <32375836+MaganMK@users.noreply.github.com>
2018-03-14 15:18:04 +0100

    masse dritt

--- src/controllers/OvelsesGrupperController.java
+++ src/controllers/OvelsesGrupperController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import database.SQLConnector;
 import javafx.fxml.FXML;
@@ -8,13 +9,14 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class OvelsesGrupperController {
+
 	
 	//Lag ny ovelsesgruppe
 	@FXML TextField newGroupField;
 
-	@FXML public void createGroup() throws SQLException {
+	@FXML public void createGroup() throws SQLException, ClassNotFoundException {
 		String newGroup = newGroupField.getText();
-		SQLConnector.insertToTable("ovelsesgruppe",newGroup);
+		SQLConnector.insertToTable("Ovelsesgruppe",newGroup);
 	}
 	
 	
@@ -23,20 +25,34 @@ public class OvelsesGrupperController {
 	@FXML TextField addExerciseField,
 					addToGroupField;
 	
-	@FXML public void addExerciseToGroup() {
+	@FXML public void addExerciseToGroup() throws ClassNotFoundException, SQLException {
 		String exercise = addExerciseField.getText();
 		String group = addToGroupField.getText();
-		//SQLConnector.addExerciseToGroup(exercise, group);
+		SQLConnector.addExerciseToGroup(exercise, group);
 	}
 	
 	
 	
 	//Vise liknende ovelser i listview
 	@FXML TextField exerciseWithGroupField;
-	@FXML ListView<?> sameGroupView; //TODO parameter
+	@FXML TextField sameGroupView; //TODO parameter
 	
-	@FXML public void showExerciseGroup() {
+	@FXML public void showExerciseGroup() throws ClassNotFoundException, SQLException {
+		String group = exerciseWithGroupField.getText();
 		
+		List<String> exercises = SQLConnector.getExercisesInGroup(group);
+		
+		sameGroupView.setText(exercises.toString());
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
--- src/database/SQLConnector.java
+++ src/database/SQLConnector.java
@@ -2,15 +2,20 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
  
 /**
  *
  * @author sqlitetutorial.net
  */
 public class SQLConnector {
-	private static String url = "jdbc:sqlite:src/database/database.db";
+	private static String url = "jdbc:mysql://mysql.stud.ntnu.no/didris_test4";
+	private static String username = "didris_test";
+	private static String password = "1234";
 	private static Connection connection;
 	
 	// Metode for aa hente en SQL connection
@@ -18,8 +23,8 @@ public class SQLConnector {
 		if(connection == null) {
 			try {
 				System.out.println("Connecting database...");
-				String url = "jdbc:sqlite:src/database/database.db";
-				connection = DriverManager.getConnection(url);
+				// Class.forName("com.mysql.jdbc.Driver");
+				connection = DriverManager.getConnection(SQLConnector.url, SQLConnector.username, SQLConnector.password);
 				System.out.println("Successfully connected to the database");
 			}
 			catch(SQLException e) {
@@ -43,12 +48,50 @@ public class SQLConnector {
 	}
 
 	// Funker for aa sette inn i ovelsesgruppe
-	public static void insertToTable(String table, String value) throws SQLException
+	public static void insertToTable(String table, String value) throws SQLException, ClassNotFoundException
 	{
 		Connection connection = SQLConnector.getConnection();
 		Statement statement = connection.createStatement();
 		
-		statement.executeUpdate("INSERT INTO\"" + table + "\" VALUES (\"" + value + "\");");
+		statement.executeUpdate("INSERT INTO " + table + " VALUES (\"" + value + "\");");
+	}
+	
+	public static void addExerciseToGroup(String exercise, String group) throws SQLException, ClassNotFoundException
+	{
+		Connection connection = SQLConnector.getConnection();
+		Statement statement = connection.createStatement();
+		
+		statement.executeUpdate("INSERT INTO OvelseIOvelsesgruppe VALUES (\"" + exercise + "\",\"" + group + "\");");
+	}
+	
+	public static List<String> getExercisesInGroup(String group) throws ClassNotFoundException, SQLException
+	{
+		List<String> result = new ArrayList<>();
+		ResultSet rs = getResultSet("SELECT * FROM `OvelseIOvelsesgruppe` WHERE ovelsesgruppe_navn =\"" + group+"\";");
+		while(rs.next()) {
+				String current = rs.getString("ovelse_navn");									
+				result.add(current);
+			
+		}
+		return result;
+	}
+	
+	public static ResultSet getResultSet(String query) throws SQLException, ClassNotFoundException {
+		Connection connection;
+		try {
+			connection = SQLConnector.getConnection();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			throw e1;
+		}
+		Statement stmt;
+		try {
+			stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			return rs;
+		} catch(SQLException e) {
+			throw e;
+		}
 	}
 }
 
