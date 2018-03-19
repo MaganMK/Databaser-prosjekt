e33ba3da1e9ca08e121db64d7523ee716a5f46ad
MaganMK <32375836+MaganMK@users.noreply.github.com>
2018-03-19 14:12:31 +0100

    iawjefioaaåowg

---
+++ src/Core/AppLauncher.java
@@ -0,0 +1,30 @@
+package Core;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class AppLauncher extends Application {
+	
+	@Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/TreningsApp.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("Prosjekt");
+        stage.setScene(scene);
+        stage.show();
+        
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
